432a8307d33463a761f0ac5a1b9c1eb4bec53dab
jsg <jsg921019@gmail.com>
2021-08-25 09:09:04 +0000

    initial commit

---
+++ inference.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import torch
+from itertools import chain
+from tqdm import tqdm
+
+def create_label(model, test_dataloader, df_submit, device, target='ans', save=False):
+   
+    model.eval()
+    ans = []
+    for inputs in tqdm(test_dataloader):
+        inputs = inputs.to(device)
+
+        with torch.no_grad():
+            outputs = model(inputs)
+            _, preds = torch.max(outputs, 1)
+            ans.append(preds.tolist())
+
+    ans = list(chain(*ans))
+    df_submit[target] = ans
+    
+    if save:
+        df_submit.to_csv('test.csv',index=False)
+        
+    print(f'inference {target} complete!')
+    return df_submit
+
+def sum_label(df):
+    
+    def _label(row):
+        return row['age'] + 3*row['gender'] + 6*row['mask']
+
+    df['ans'] = df.apply(_label, axis=1)
+    return df[['ImageID', 'ans']]
+    
